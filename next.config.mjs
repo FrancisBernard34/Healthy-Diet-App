e3169338a9452c5d3b7f1598dedba9dfd639e1a5
Francis Bernard <francisbernardcontato@gmail.com>
2024-03-01 21:48:39 -0300

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Adiciona um alias para o diretório raiz do projeto
+    config.resolve.alias["@"] = path.resolve(__dirname);
+
+    return config;
+  },
+};
 
 export default nextConfig;
