c89bea48818949df4c1779a50a84f0fcdd71378f
Francis Bernard <francisbernardcontato@gmail.com>
2024-03-01 23:46:28 -0300

    update api production url

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,5 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
+
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://healthy-diet-app.vercel.app"
+    : "http://localhost:3000";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: url,
 });
