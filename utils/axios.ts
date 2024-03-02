2c83c0c448c52563f7f83cb1fab91e2b726b9b1c
Francis Bernard <francisbernardcontato@gmail.com>
2024-03-02 00:25:53 -0300

    release version 0.1.2

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   // change this to your server's URL
-  baseURL: "https://healthy-diet-api.vercel.app",
+  baseURL: "https://healthy-diet-app.vercel.app",
 });
